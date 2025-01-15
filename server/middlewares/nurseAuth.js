93fd3c3fc3538be30ff30c86906d450f9ab68485
arjun-saseendran <arjunsaseendran@icloud.com>
2025-01-15 21:15:45 +0530

    add nurseAuth

--- server/middlewares/nurseAuth.js
+++ server/middlewares/nurseAuth.js
@@ -1,13 +1,29 @@
-import jwt from 'jsonwebtoken';
-import {catchErrorHandler} from '../utils/catchErrorHandler.js';
+import jwt from "jsonwebtoken";
+import { catchErrorHandler } from "../utils/catchErrorHandler.js";
 
 export const nurseAuth = (req, res, next) => {
-    try {
-        // Get token
-        const {token} = req.cookies;
+  try {
+    // Get token
+    const { token } = req.cookies;
 
-        
-    } catch (error) {
-        
+    // Handle no token
+    if (!token) {
+      return res.status(401).json({ message: "Token not provided!" });
     }
-}
+
+    // Decode token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Handle no decoded token
+    if (!decoded) {
+      return res.status(401).json({ message: "Nurse not authorized!" });
+    }
+
+    // Set nurse
+    req.user = decoded;
+    next();
+  } catch (error) {
+    // Handle catch error
+    catchErrorHandler(res, error);
+  }
+};
